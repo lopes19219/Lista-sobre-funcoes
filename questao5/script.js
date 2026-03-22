fecd49d4d49c4799b7c60124a5dfa6749954702a
lopes19219 <gcjlopes@gmail.com>
2026-03-22 14:00:06 -0300

    Adiciona JS da questão 5

---
+++ questao5/script.js
@@ -0,0 +1,7 @@
+function calcularMedia(){ 
+    let nota1 = Number (prompt('Digite a primeira nota'));
+    let nota2 = Number (prompt('Digite a segunda nota'));
+    let nota3 = Number (prompt('Digite a terceira nota'));
+    let media = (nota1 + nota2 + nota3) / 3;
+    document.writeln('Sua média é: ' + media);
+}
