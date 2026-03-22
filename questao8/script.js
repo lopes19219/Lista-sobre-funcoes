4b7f79d50eecb95a0f6a3f5764f6b299d73a5115
lopes19219 <gcjlopes@gmail.com>
2026-03-22 14:06:05 -0300

    Adiciona JS da questão 8

---
+++ questao8/script.js
@@ -0,0 +1,5 @@
+function converterMedida(){
+    let metros = Number(prompt('Digite o valor em metros:'));
+    let centimetros = metros * 100;
+    document.writeln(metros + ' metros = ' + centimetros + ' centímetros');
+}
