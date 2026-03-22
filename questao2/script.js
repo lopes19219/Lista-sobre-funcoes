87672de8bd4843b004ba1ffee66dd1be5752fa2d
lopes19219 <gcjlopes@gmail.com>
2026-03-22 13:52:25 -0300

    Adiciona JS da questão 2

---
+++ questao2/script.js
@@ -0,0 +1,6 @@
+function calcularAreaTriangulo(){
+    let base = Number (prompt('Digite a base'));
+    let altura = Number (prompt('Digite a altura'));
+    let area = (base * altura) / 2;
+    document.writeln('A área do triângulo é: ' + area);
+}
